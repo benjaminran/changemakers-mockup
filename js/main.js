730649615060cc9ab48360b6181ec21a6185d17c
benjaminran <benjaminran2@gmail.com>
2015-09-27 18:07:26 -0700

    last touches

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
     // Scrolling
-    $('.scroll').click(function() {
-        $('body').animate({
-            scrollTop: eval($('#' + $(this).attr('target')).offset().top - 70)
-        }, 500);
-    });
+    /*$('.scroll').click(function() {
+     $('body').animate({
+     scrollTop: eval($('#' + $(this).attr('target')).offset().top - 70)
+     }, 500);
+     });*/
+    /*var hashTagActive = "";
+    $(".scroll").click(function (event) {
+        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 1000, 'swing');
+            hashTagActive = this.hash;
+        }
+    });*/
 });
